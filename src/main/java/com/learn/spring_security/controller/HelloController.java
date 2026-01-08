2f8a89cb4d672ca07521237167836af0df7db1ff
Developer <pawanraj.kushwah.javarnd@gmail.com>
2026-01-09 02:41:07 +0530

    User Auth From database

--- src/main/java/com/learn/spring_security/controller/HelloController.java
+++ src/main/java/com/learn/spring_security/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.learn.spring_security;
+package com.learn.spring_security.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
